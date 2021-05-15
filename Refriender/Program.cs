8b65c01b22ecbc767fdf7142564f0b2a6b2a270c
Sera Brocious <sera.brocious@gmail.com>
2021-05-14 23:07:53 -0400

    Switched to classless C#9 entrypoint. Fixed bug with DataBytes going out of bounds.

--- Refriender/Program.cs
+++ Refriender/Program.cs
@@ -9,178 +9,171 @@ using HeyRed.Mime;
 using MoreLinq;
 using RefrienderCore;
 
-namespace Refriender {
-	class Program {
-		static readonly Dictionary<string, CompressionAlgorithm> Algorithms = new() {
-			["deflate"] = CompressionAlgorithm.Deflate, 
-			["zlib"] = CompressionAlgorithm.Zlib,
-			["gzip"] = CompressionAlgorithm.Gzip,
-			["bzip2"] = CompressionAlgorithm.Bzip2, 
-			["lzw"] = CompressionAlgorithm.Lzw, 
-			["lz4raw"] = CompressionAlgorithm.Lz4Raw, 
-			["lz4frame"] = CompressionAlgorithm.Lz4Frame, 
-			["all"] = CompressionAlgorithm.All, 
-		};
+Dictionary<string, CompressionAlgorithm> Algorithms = new() {
+	["deflate"] = CompressionAlgorithm.Deflate, 
+	["zlib"] = CompressionAlgorithm.Zlib,
+	["gzip"] = CompressionAlgorithm.Gzip,
+	["bzip2"] = CompressionAlgorithm.Bzip2, 
+	["lzw"] = CompressionAlgorithm.Lzw, 
+	["lz4raw"] = CompressionAlgorithm.Lz4Raw, 
+	["lz4frame"] = CompressionAlgorithm.Lz4Frame, 
+	["all"] = CompressionAlgorithm.All, 
+};
 
-		static readonly Dictionary<CompressionAlgorithm, string> RevAlgorithms =
-			Algorithms.ToDictionary(x => x.Value, x => x.Key);
+var RevAlgorithms = Algorithms.ToDictionary(x => x.Value, x => x.Key);
 
-		public class Options {
-			[Option('q', "quiet", Required = false, HelpText = "Silence messages")]
-			public bool Quiet { get; set; }
-			[Option('v', "verbose", Required = false, HelpText = "Verbose messages")]
-			public bool Verbose { get; set; }
-			[Option('s', "start-only", Required = false, HelpText = "Only find starting positions of blocks")]
-			public bool StartOnly { get; set; }
-			[Option('i', "identify", Required = false, HelpText = "Look for magic in decompressed blocks")]
-			public bool Identify { get; set; }
-			[Option("read-file", Required = false, HelpText = "Read entire file into memory instead of mapping (Warning: ONLY works for files <=2GB)")]
-			public bool ReadFile { get; set; }
-			[Option('p', "preserve-overlapping", Required = false, HelpText = "Preserve overlapping blocks")]
-			public bool PreserveOverlapping { get; set; }
-			[Option('e', "extract-to", Required = false, HelpText = "Path for extraction")]
-			public string ExtractTo { get; set; }
-			[Option('a', "algorithms", Required = false, Default = "all", HelpText = "Comma-separated list of algorithms (valid options: all, deflate, zlib, gzip, bzip2, lzw, lz4raw, lz4frame)")]
-			public string Algorithms { get; set; }
-			[Option('f', "find-pointers", Required = false, HelpText = "Comma-separated list of offsets/ranges in which to search for pointers to blocks (e.g. 0,4,8,16 or 1-8,32)")]
-			public string FindPointers { get; set; }
-			[Option('F', "find-end-pointers", Required = false, HelpText = "Comma-separated list of offsets/ranges in which to search for pointers to the end of blocks (e.g. 0,4,8,16 or 1-8,32)")]
-			public string FindEndPointers { get; set; }
-			[Option('m', "min-length", Required = false, Default = 128, HelpText = "Minimum decompressed block length in bytes")]
-			public int MinLength { get; set; }
-			[Value(0, Required = true, MetaName = "filename", HelpText = "File to scan")]
-			public string Filename { get; set; }
+void DisplayHelp(ParserResult<Options> result) {
+	Console.Error.WriteLine(HelpText.AutoBuild(result, h => {
+		h.AdditionalNewLineAfterOption = false;
+		h.Heading = $"Refriender {typeof(Options).Assembly.GetName().Version}";
+		h.Copyright = "Copyright (c) 2021 Serafina Brocious";
+		h.AutoVersion = false;
+		return HelpText.DefaultParsingErrorsHandler(result, h);
+	}, e => e));
+	Environment.Exit(1);
+}
+
+var result = new Parser(with => with.HelpWriter = null).ParseArguments<Options>(args);
+result.WithNotParsed(errs => DisplayHelp(result))
+.WithParsed(opt => {
+	if(opt.Quiet && (opt.StartOnly || opt.Verbose || opt.Identify || opt.FindPointers != null)) {
+		Console.Error.Write("ERROR: -q/--quiet cannot be combined with ");
+		Console.Error.WriteLine(true switch {
+			_ when opt.StartOnly => "-s/--start-only", 
+			_ when opt.Verbose => "-v/--verbose", 
+			_ when opt.Identify => "-i/--identify",
+			_ when opt.FindPointers != null => "-f/--find-pointers", 
+			_ when opt.FindEndPointers != null => "-F/--find-end-pointers", 
+			_ => throw new NotSupportedException()
+		});
+		Environment.Exit(1);
+	}
+	if(opt.Quiet && opt.ExtractTo == null) {
+		Console.Error.WriteLine("ERROR: -q/--quiet cannot be used without -e/extract-to");
+		Environment.Exit(1);
+	}
+
+	if(!File.Exists(opt.Filename)) {
+		Console.Error.WriteLine($"ERROR: Could not find file: '{opt.Filename}'");
+		Environment.Exit(1);
+	}
+
+	var algorithms = (CompressionAlgorithm) opt.Algorithms.Split(',')
+		.Select(x => x.Trim().ToLower()).Where(x => x.Length != 0)
+		.Select(x => {
+			if(!Algorithms.TryGetValue(x, out var flag)) {
+				Console.Error.WriteLine($"ERROR: Unknown compression algorithm '{x}'");
+				Environment.Exit(1);
+			}
+			return (int) flag;
+		}).Sum();
+	
+	IData data = opt.ReadFile
+		? new DataBytes(File.ReadAllBytes(opt.Filename))
+		: new DataMapped(opt.Filename);
+	var cf = new CompressionFinder(data, minLength: opt.MinLength, algorithms: algorithms,
+		positionOnly: opt.StartOnly, removeOverlapping: !opt.PreserveOverlapping,
+		logLevel: opt.Quiet ? 0 : opt.Verbose ? 2 : 1);
+	if(opt.StartOnly) {
+		foreach(var (algorithm, offset) in cf.StartingPositions.OrderBy(x => x.Offset))
+			Console.WriteLine($"[{RevAlgorithms[algorithm]}] Block starts at 0x{offset:X}");
+		Console.WriteLine($"{cf.StartingPositions.Count} starting positions found");
+	} else {
+		if(!opt.Quiet) {
+			foreach(var block in cf.Blocks.OrderBy(x => x.Offset))
+				Console.WriteLine($"[{RevAlgorithms[block.Algorithm]}] 0x{block.Offset:X} - 0x{block.Offset + block.CompressedLength:X} (compressed length 0x{block.CompressedLength:X}, decompressed length 0x{block.DecompressedLength:X})");
+			Console.WriteLine($"{cf.Blocks.Count} blocks found");
 		}
 
-		static void DisplayHelp(ParserResult<Options> result) {
-			Console.Error.WriteLine(HelpText.AutoBuild(result, h => {
-				h.AdditionalNewLineAfterOption = false;
-				h.Heading = $"Refriender {typeof(Program).Assembly.GetName().Version}";
-				h.Copyright = "Copyright (c) 2021 Serafina Brocious";
-				h.AutoVersion = false;
-				return HelpText.DefaultParsingErrorsHandler(result, h);
-			}, e => e));
-			Environment.Exit(1);
+		void DoFind(string spec, bool isEnd) {
+			var offsets = spec.Split(',').Select(x => {
+				try {
+					var a = x.Trim().Split('-');
+					var start = int.Parse(a[0]);
+					return a.Length == 2
+						? Enumerable.Range(start, int.Parse(a[1]) - start + 1)
+						: new[] { start };
+				} catch(Exception) {
+					Console.Error.WriteLine(isEnd
+						? $"ERROR: Invalid offset string for -F/--find-end-pointers: {x.Trim()}"
+						: $"ERROR: Invalid offset string for -f/--find-pointers: {x.Trim()}");
+					Environment.Exit(1);
+					throw;
+				}
+			}).SelectMany(x => x);
+			foreach(var offset in offsets) {
+				if(opt.Verbose && isEnd)
+					Console.WriteLine($"Finding pointers to {offset} bytes after the blocks");
+				else if(opt.Verbose)
+					Console.WriteLine(offset == 0
+						? "Finding pointers to blocks"
+						: $"Finding pointers to {offset} bytes before the blocks");
+				var bpointers = cf.Blocks.AsParallel()
+					.Select(x => (x, cf.FindPointers(isEnd ? x.Offset + x.CompressedLength + offset : x.Offset - offset)))
+					.Where(x => x.Item2.Count != 0)
+					.OrderBy(x => x.Item2.First()).ToList();
+				foreach(var (block, pointers) in bpointers)
+					if(isEnd)
+						Console.WriteLine($"Block 0x{block.Offset:X}-0x{block.Offset+block.CompressedLength:X}" + 
+						                  $"{(offset != 0 ? $" (+ {offset} == 0x{block.Offset+block.CompressedLength + offset:X})" : "")} " +
+						                  $"has end pointers from: {string.Join(", ", pointers.Select(x => $"0x{x:X}"))}");
+					else
+						Console.WriteLine($"Block 0x{block.Offset:X}{(offset != 0 ? $" (- {offset} == 0x{block.Offset - offset:X})" : "")} " +
+						                  $"has pointers from: {string.Join(", ", pointers.Select(x => $"0x{x:X}"))}");
+				Console.WriteLine(
+					$"{(isEnd ? "End p" : "P")}ointers with offset {offset}: {bpointers.Select(x => x.Item2.Count).Sum()}");
+			}
 		}
-		
-		static void Main(string[] args) {
-			var result = new Parser(with => with.HelpWriter = null).ParseArguments<Options>(args);
-			result.WithNotParsed(errs => DisplayHelp(result))
-			.WithParsed(opt => {
-				if(opt.Quiet && (opt.StartOnly || opt.Verbose || opt.Identify || opt.FindPointers != null)) {
-					Console.Error.Write("ERROR: -q/--quiet cannot be combined with ");
-					Console.Error.WriteLine(true switch {
-						_ when opt.StartOnly => "-s/--start-only", 
-						_ when opt.Verbose => "-v/--verbose", 
-						_ when opt.Identify => "-i/--identify",
-						_ when opt.FindPointers != null => "-f/--find-pointers", 
-						_ when opt.FindEndPointers != null => "-F/--find-end-pointers", 
-						_ => throw new NotSupportedException()
-					});
-					Environment.Exit(1);
-				}
-				if(opt.Quiet && opt.ExtractTo == null) {
-					Console.Error.WriteLine("ERROR: -q/--quiet cannot be used without -e/extract-to");
-					Environment.Exit(1);
-				}
+		if(opt.FindPointers != null) DoFind(opt.FindPointers, false);
+		if(opt.FindEndPointers != null) DoFind(opt.FindEndPointers, true);
 
-				if(!File.Exists(opt.Filename)) {
-					Console.Error.WriteLine($"ERROR: Could not find file: '{opt.Filename}'");
-					Environment.Exit(1);
-				}
-
-				var algorithms = (CompressionAlgorithm) opt.Algorithms.Split(',')
-					.Select(x => x.Trim().ToLower()).Where(x => x.Length != 0)
-					.Select(x => {
-						if(!Algorithms.TryGetValue(x, out var flag)) {
-							Console.Error.WriteLine($"ERROR: Unknown compression algorithm '{x}'");
-							Environment.Exit(1);
-						}
-						return (int) flag;
-					}).Sum();
-				
-				IData data = opt.ReadFile
-					? new DataBytes(File.ReadAllBytes(opt.Filename))
-					: new DataMapped(opt.Filename);
-				var cf = new CompressionFinder(data, minLength: opt.MinLength, algorithms: algorithms,
-					positionOnly: opt.StartOnly, removeOverlapping: !opt.PreserveOverlapping,
-					logLevel: opt.Quiet ? 0 : opt.Verbose ? 2 : 1);
-				if(opt.StartOnly) {
-					foreach(var (algorithm, offset) in cf.StartingPositions.OrderBy(x => x.Offset))
-						Console.WriteLine($"[{RevAlgorithms[algorithm]}] Block starts at 0x{offset:X}");
-					Console.WriteLine($"{cf.StartingPositions.Count} starting positions found");
-				} else {
-					if(!opt.Quiet) {
-						foreach(var block in cf.Blocks.OrderBy(x => x.Offset))
-							Console.WriteLine($"[{RevAlgorithms[block.Algorithm]}] 0x{block.Offset:X} - 0x{block.Offset + block.CompressedLength:X} (compressed length 0x{block.CompressedLength:X}, decompressed length 0x{block.DecompressedLength:X})");
-						Console.WriteLine($"{cf.Blocks.Count} blocks found");
-					}
-
-					void DoFind(string spec, bool isEnd) {
-						var offsets = spec.Split(',').Select(x => {
-							try {
-								var a = x.Trim().Split('-');
-								var start = int.Parse(a[0]);
-								return a.Length == 2
-									? Enumerable.Range(start, int.Parse(a[1]) - start + 1)
-									: new[] { start };
-							} catch(Exception) {
-								Console.Error.WriteLine(isEnd
-									? $"ERROR: Invalid offset string for -F/--find-end-pointers: {x.Trim()}"
-									: $"ERROR: Invalid offset string for -f/--find-pointers: {x.Trim()}");
-								Environment.Exit(1);
-								throw;
-							}
-						}).SelectMany(x => x);
-						foreach(var offset in offsets) {
-							if(opt.Verbose && isEnd)
-								Console.WriteLine($"Finding pointers to {offset} bytes after the blocks");
-							else if(opt.Verbose)
-								Console.WriteLine(offset == 0
-									? "Finding pointers to blocks"
-									: $"Finding pointers to {offset} bytes before the blocks");
-							var bpointers = cf.Blocks.AsParallel()
-								.Select(x => (x, cf.FindPointers(isEnd ? x.Offset + x.CompressedLength + offset : x.Offset - offset)))
-								.Where(x => x.Item2.Count != 0)
-								.OrderBy(x => x.Item2.First()).ToList();
-							foreach(var (block, pointers) in bpointers)
-								if(isEnd)
-									Console.WriteLine($"Block 0x{block.Offset:X}-0x{block.Offset+block.CompressedLength:X}" + 
-									                  $"{(offset != 0 ? $" (+ {offset} == 0x{block.Offset+block.CompressedLength + offset:X})" : "")} " +
-									                  $"has end pointers from: {string.Join(", ", pointers.Select(x => $"0x{x:X}"))}");
-								else
-									Console.WriteLine($"Block 0x{block.Offset:X}{(offset != 0 ? $" (- {offset} == 0x{block.Offset - offset:X})" : "")} " +
-									                  $"has pointers from: {string.Join(", ", pointers.Select(x => $"0x{x:X}"))}");
-							Console.WriteLine(
-								$"{(isEnd ? "End p" : "P")}ointers with offset {offset}: {bpointers.Select(x => x.Item2.Count).Sum()}");
-						}
-					}
-					if(opt.FindPointers != null) DoFind(opt.FindPointers, false);
-					if(opt.FindEndPointers != null) DoFind(opt.FindEndPointers, true);
-
-					if(opt.ExtractTo == null && !opt.Identify) return;
-					if(opt.Identify) {
-						if(opt.Verbose) Console.WriteLine("Beginning block identification");
-						cf.Blocks.AsParallel().Select(block => {
-							var bdata = cf.Decompress(block);
-							var magic = new Magic(MagicOpenFlags.MAGIC_NONE);
-							return (Block: block, Magic: magic.Read(bdata, bdata.Length));
-						}).Where(x => x.Magic != "data").OrderBy(x => x.Block.Offset).ForEach(x =>
-							Console.WriteLine($"[{RevAlgorithms[x.Block.Algorithm]}] 0x{x.Block.Offset:X} - 0x{x.Block.Offset + x.Block.CompressedLength:X} (decompressed length 0x{x.Block.DecompressedLength:X}): {x.Magic}"));
-					}
-					if(opt.ExtractTo != null) {
-						if(opt.Verbose) Console.WriteLine("Beginning block extraction");
-						Directory.CreateDirectory(opt.ExtractTo);
-						Parallel.ForEach(cf.Blocks, block => {
-							var fn =
-								$"0x{block.Offset:X}-0x{block.Offset + block.CompressedLength:X}_{RevAlgorithms[block.Algorithm]}.bin";
-							var bdata = cf.Decompress(block);
-							File.WriteAllBytes(Path.Join(opt.ExtractTo, fn), bdata);
-						});
-						if(opt.Verbose) Console.WriteLine("Done!");
-					}
-				}
+		if(opt.ExtractTo == null && !opt.Identify) return;
+		if(opt.Identify) {
+			if(opt.Verbose) Console.WriteLine("Beginning block identification");
+			cf.Blocks.AsParallel().Select(block => {
+				var bdata = cf.Decompress(block);
+				var magic = new Magic(MagicOpenFlags.MAGIC_NONE);
+				return (Block: block, Magic: magic.Read(bdata, bdata.Length));
+			}).Where(x => x.Magic != "data").OrderBy(x => x.Block.Offset).ForEach(x =>
+				Console.WriteLine($"[{RevAlgorithms[x.Block.Algorithm]}] 0x{x.Block.Offset:X} - 0x{x.Block.Offset + x.Block.CompressedLength:X} (decompressed length 0x{x.Block.DecompressedLength:X}): {x.Magic}"));
+		}
+		if(opt.ExtractTo != null) {
+			if(opt.Verbose) Console.WriteLine("Beginning block extraction");
+			Directory.CreateDirectory(opt.ExtractTo);
+			Parallel.ForEach(cf.Blocks, block => {
+				var fn =
+					$"0x{block.Offset:X}-0x{block.Offset + block.CompressedLength:X}_{RevAlgorithms[block.Algorithm]}.bin";
+				var bdata = cf.Decompress(block);
+				File.WriteAllBytes(Path.Join(opt.ExtractTo, fn), bdata);
 			});
+			if(opt.Verbose) Console.WriteLine("Done!");
 		}
 	}
+});
+
+class Options {
+	[Option('q', "quiet", Required = false, HelpText = "Silence messages")]
+	public bool Quiet { get; set; }
+	[Option('v', "verbose", Required = false, HelpText = "Verbose messages")]
+	public bool Verbose { get; set; }
+	[Option('s', "start-only", Required = false, HelpText = "Only find starting positions of blocks")]
+	public bool StartOnly { get; set; }
+	[Option('i', "identify", Required = false, HelpText = "Look for magic in decompressed blocks")]
+	public bool Identify { get; set; }
+	[Option("read-file", Required = false, HelpText = "Read entire file into memory instead of mapping (Warning: ONLY works for files <=2GB)")]
+	public bool ReadFile { get; set; }
+	[Option('p', "preserve-overlapping", Required = false, HelpText = "Preserve overlapping blocks")]
+	public bool PreserveOverlapping { get; set; }
+	[Option('e', "extract-to", Required = false, HelpText = "Path for extraction")]
+	public string ExtractTo { get; set; }
+	[Option('a', "algorithms", Required = false, Default = "all", HelpText = "Comma-separated list of algorithms (valid options: all, deflate, zlib, gzip, bzip2, lzw, lz4raw, lz4frame)")]
+	public string Algorithms { get; set; }
+	[Option('f', "find-pointers", Required = false, HelpText = "Comma-separated list of offsets/ranges in which to search for pointers to blocks (e.g. 0,4,8,16 or 1-8,32)")]
+	public string FindPointers { get; set; }
+	[Option('F', "find-end-pointers", Required = false, HelpText = "Comma-separated list of offsets/ranges in which to search for pointers to the end of blocks (e.g. 0,4,8,16 or 1-8,32)")]
+	public string FindEndPointers { get; set; }
+	[Option('m', "min-length", Required = false, Default = 128, HelpText = "Minimum decompressed block length in bytes")]
+	public int MinLength { get; set; }
+	[Value(0, Required = true, MetaName = "filename", HelpText = "File to scan")]
+	public string Filename { get; set; }
 }
--- RefrienderCore/DataBytes.cs
+++ RefrienderCore/DataBytes.cs
@@ -9,6 +9,6 @@ namespace RefrienderCore {
 		public long Length => Data.Length;
 
 		public ReadOnlyMemory<byte> Slice(long start, long? length) =>
-			new(Data, (int) start, (int) (length ?? Data.Length - start));
+			new(Data, (int) start, (int) Math.Min(length ?? Data.Length - start, Data.Length - start));
 	}
 }
